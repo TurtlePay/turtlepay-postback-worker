d2511bae571f222653d8ec05b0f5dc72124372bd
Brandon Lehmann <brandonlehmann@gmail.com>
2019-09-29 16:30:29 -0400

    general code updates including refactoring, cleanup, better logging, etc

--- index.js
+++ index.js
@@ -5,185 +5,160 @@
 'use strict'
 
 require('dotenv').config()
-const Config = require('./config.json')
-const RabbitMQ = require('amqplib')
 const cluster = require('cluster')
-const util = require('util')
-const request = require('request-promise-native')
+require('colors')
+const Config = require('./config.json')
+const cpuCount = Math.ceil(require('os').cpus().length / 8)
+const Helpers = require('./lib/helpers.js')
 const MessageSigner = require('./lib/messageSigner.js')
+const RabbitMQ = require('./lib/rabbit.js')
+const request = require('request-promise-native')
 const signer = new MessageSigner()
-
-const cpuCount = require('os').cpus().length
-
-const publicRabbitHost = process.env.RABBIT_PUBLIC_SERVER || 'localhost'
-const publicRabbitUsername = process.env.RABBIT_PUBLIC_USERNAME || ''
-const publicRabbitPassword = process.env.RABBIT_PUBLIC_PASSWORD || ''
-
-function log (message) {
-  console.log(util.format('%s: %s', (new Date()).toUTCString(), message))
-}
+const util = require('util')
 
 function spawnNewWorker () {
   cluster.fork()
 }
 
-/* Helps us to build the RabbitMQ connection string */
-function buildConnectionString (host, username, password) {
-  log(util.format('Setting up connection to %s@%s...', username, host))
-  var result = ['amqp://']
-
-  if (username.length !== 0 && password.length !== 0) {
-    result.push(username + ':')
-    result.push(password + '@')
+if (cluster.isMaster) {
+  if (!process.env.NODE_ENV || process.env.NODE_ENV.toLowerCase() !== 'production') {
+    Helpers.log('[WARNING] Node.js is not running in production mode. Consider running in production mode: export NODE_ENV=production'.yellow)
   }
 
-  result.push(host)
-
-  return result.join('')
-}
-
-if (cluster.isMaster) {
-  console.log('Starting TurtlePay Postback Service...')
+  Helpers.log('Starting TurtlePay Postback Service...')
 
   for (var cpuThread = 0; cpuThread < cpuCount; cpuThread++) {
     spawnNewWorker()
   }
 
   cluster.on('exit', (worker, code, signal) => {
-    log(util.format('worker %s died', worker.process.pid))
+    Helpers.log(util.format('worker %s died', worker.process.pid))
     spawnNewWorker()
   })
 } else if (cluster.isWorker) {
-  (async function () {
-    try {
-      /* Set up our access to the necessary RabbitMQ systems */
-      var publicRabbit = await RabbitMQ.connect(buildConnectionString(publicRabbitHost, publicRabbitUsername, publicRabbitPassword))
-      var publicChannel = await publicRabbit.createChannel()
+  const rabbit = new RabbitMQ(process.env.RABBIT_PUBLIC_SERVER || 'localhost', process.env.RABBIT_PUBLIC_USERNAME || '', process.env.RABBIT_PUBLIC_PASSWORD || '', false)
 
-      publicRabbit.on('error', (error) => {
-        throw new Error(error)
-      })
-      publicChannel.on('error', (error) => {
-        throw new Error(error)
-      })
+  rabbit.on('log', log => {
+    Helpers.log(util.format('[RABBIT] %s', log))
+  })
 
-      await publicChannel.assertQueue(Config.queues.complete, {
-        durable: true
-      })
+  rabbit.on('connect', () => {
+    Helpers.log(util.format('[RABBIT] connected to server at %s', process.env.RABBIT_PUBLIC_SERVER || 'localhost'))
+  })
 
-      publicChannel.prefetch(1)
+  rabbit.on('disconnect', (error) => {
+    Helpers.log(util.format('[RABBIT] lost connected to server: %s', error.toString()))
+    cluster.worker.kill()
+  })
 
-      /* Looks like we received a request */
-      publicChannel.consume(Config.queues.complete, async function (message) {
-        if (message !== null) {
-          /* Parse the incoming message */
-          var payload = JSON.parse(message.content.toString())
-
-          if (!payload.request.callback) {
-            /* Caller did not provide a callback */
-            log(util.format('Worker #%s: Caller did not provide a callback for %s', cluster.worker.id, payload.address))
-            return publicChannel.ack(message)
-          }
-
-          /* Build what we're going to try to send back */
-          const postbackPayload = {
-            address: payload.address,
-            status: payload.status,
-            request: {
-              address: payload.request.address,
-              amount: payload.request.amount,
-              userDefined: payload.request.callerData
-            }
-          }
-
-          /* If we have a transaction hash add that in */
-          if (payload.transactionHash) {
-            postbackPayload.txnHash = payload.transactionHash
-
-            /* If we also have a transaction private key, let's send that back as well */
-            if (payload.transactionPrivateKey) {
-              postbackPayload.txnPrivateKey = payload.transactionPrivateKey
-            }
-          }
-
-          /* If we have an amountReceived, add that in */
-          if (payload.amountReceived) {
-            postbackPayload.amountReceived = payload.amountReceived
-          }
-
-          /* If we have an amountSent, add that in */
-          if (payload.amountSent) {
-            postbackPayload.amountSent = payload.amountSent
-            postbackPayload.amount = payload.amountSent // This is provided for legacy support
-          }
-
-          /* If we have a networkFee, add that in */
-          if (payload.networkFee) {
-            postbackPayload.networkFee = payload.networkFee
-          }
-
-          /* If we have an amount, add that in */
-          if (payload.amount) {
-            postbackPayload.amount = payload.amount
-          }
-
-          /* If we have blocksRemaining add that in */
-          if (payload.blocksRemaining) {
-            postbackPayload.blocksRemaining = payload.blocksRemaining
-          }
-
-          /* If we have confirmationsRemaining add that in */
-          if (payload.confirmationsRemaining) {
-            postbackPayload.confirmationsRemaining = payload.confirmationsRemaining
-          }
-
-          /* If we have the keys for the one-time use wallet, add that in */
-          if (payload.keys) {
-            postbackPayload.keys = payload.keys
-          }
-
-          /* If we have a URL that we can post to, then we're going to give it a try */
-          if (payload.request.callback.substring(0, 4).toLowerCase() === 'http') {
-            request({
-              url: payload.request.callback,
-              method: 'POST',
-              json: true,
-              body: postbackPayload,
-              headers: {
-                digest: util.format('SHA-256=', signer.digest(postbackPayload, 'base64'))
-              },
-              httpSignature: {
-                algorithm: 'rsa-sha256',
-                headers: [
-                  '(request-target)',
-                  'date',
-                  'digest'
-                ],
-                keyId: postbackPayload.address,
-                key: Buffer.from(payload.privateKey, 'hex')
-              },
-              timeout: Config.postTimeout
-            }).then(() => {
-              /* Success, we posted the message to the caller */
-              log(util.format('Worker #%s: Successfully delivered [%s] message for %s to %s', cluster.worker.id, payload.status, payload.address, payload.request.callback))
-              return publicChannel.ack(message)
-            }).catch(() => {
-              /* Success, we posted the message to the caller */
-              log(util.format('Worker #%s: Failed to deliver [%s] message for %s  to %s', cluster.worker.id, payload.status, payload.address, payload.request.callback))
-              return publicChannel.ack(message)
-            })
-          } else {
-            /* They didn't supply a valid callback, we're done here */
-            log(util.format('Worker #%s: No valid callback location available for processed payment to %s [%s]', cluster.worker.id, payload.address, payload.status))
-            return publicChannel.ack(message)
-          }
-        }
-      })
-    } catch (e) {
-      log(util.format('Error in worker #%s: %s', cluster.worker.id, e.toString()))
-      cluster.worker.kill()
+  rabbit.on('message', (queue, message, payload) => {
+    if (!payload.request.callback) {
+      Helpers.log(util.format('Worker #%s: Caller did not provide a callback for %s', cluster.worker.id, payload.address).red)
+      return rabbit.ack(message)
     }
 
-    log(util.format('Worker #%s awaiting requests', cluster.worker.id))
-  }())
+    if (payload.request.callback.substring(0, 4).toLowerCase() !== 'http') {
+      /* They didn't supply a valid callback, we're done here */
+      Helpers.log(util.format('Worker #%s: No valid callback location available for processed payment to %s [%s]', cluster.worker.id, payload.address, payload.status).red)
+      return rabbit.ack(message)
+    }
+
+    /* Build what we're going to try to send back */
+    const postbackPayload = {
+      address: payload.address,
+      status: payload.status,
+      request: {
+        address: payload.request.address,
+        amount: payload.request.amount,
+        userDefined: payload.request.callerData
+      }
+    }
+
+    /* If we have a transaction hash add that in */
+    if (payload.transactionHash) {
+      postbackPayload.txnHash = payload.transactionHash
+
+      /* If we also have a transaction private key, let's send that back as well */
+      if (payload.transactionPrivateKey) {
+        postbackPayload.txnPrivateKey = payload.transactionPrivateKey
+      }
+    }
+
+    /* If we have an amountReceived, add that in */
+    if (payload.amountReceived) {
+      postbackPayload.amountReceived = payload.amountReceived
+    }
+
+    /* If we have an amountSent, add that in */
+    if (payload.amountSent) {
+      postbackPayload.amountSent = payload.amountSent
+      postbackPayload.amount = payload.amountSent // This is provided for legacy support
+    }
+
+    /* If we have a networkFee, add that in */
+    if (payload.networkFee) {
+      postbackPayload.networkFee = payload.networkFee
+    }
+
+    /* If we have an amount, add that in */
+    if (payload.amount) {
+      postbackPayload.amount = payload.amount
+    }
+
+    /* If we have blocksRemaining add that in */
+    if (payload.blocksRemaining) {
+      postbackPayload.blocksRemaining = payload.blocksRemaining
+    }
+
+    /* If we have confirmationsRemaining add that in */
+    if (payload.confirmationsRemaining) {
+      postbackPayload.confirmationsRemaining = payload.confirmationsRemaining
+    }
+
+    /* If we have the keys for the one-time use wallet, add that in */
+    if (payload.keys) {
+      postbackPayload.keys = payload.keys
+    }
+
+    /* If we have a URL that we can post to, then we're going to give it a try */
+    request({
+      url: payload.request.callback,
+      method: 'POST',
+      json: true,
+      body: postbackPayload,
+      headers: {
+        digest: util.format('SHA-256=', signer.digest(postbackPayload, 'base64'))
+      },
+      httpSignature: {
+        algorithm: 'rsa-sha256',
+        headers: [
+          '(request-target)',
+          'date',
+          'digest'
+        ],
+        keyId: postbackPayload.address,
+        key: Buffer.from(payload.privateKey, 'hex')
+      },
+      timeout: Config.postTimeout
+    }).then(() => {
+      /* Success, we posted the message to the caller */
+      Helpers.log(util.format('Worker #%s: Successfully delivered [%s] message for %s to %s', cluster.worker.id, payload.status, payload.address, payload.request.callback).green)
+      return rabbit.ack(message)
+    }).catch(() => {
+      /* Success, we posted the message to the caller */
+      Helpers.log(util.format('Worker #%s: Failed to deliver [%s] message for %s  to %s', cluster.worker.id, payload.status, payload.address, payload.request.callback).yellow)
+      return rabbit.ack(message)
+    })
+  })
+
+  rabbit.connect().then(() => {
+    return rabbit.createQueue(Config.queues.complete, true)
+  }).then(() => {
+    return rabbit.registerConsumer(Config.queues.complete, 1)
+  }).then(() => {
+    Helpers.log(util.format('Worker #%s awaiting requests', cluster.worker.id))
+  }).catch((error) => {
+    Helpers.log(util.format('Error in worker #%s: %s', cluster.worker.id, error.toString()))
+    cluster.worker.kill()
+  })
 }
